77aa7fbb3e33957c61ebe29b5fcbe1a3f9280d97
David Rau <david.rau@hotmail.de>
2018-09-18 12:06:34 +0200

    fix last layer bug

---
+++ assignment1/code/train_pytorch.sh
@@ -0,0 +1,41 @@
+python3 train_mlp_pytorch.py --learning_rate 0.002 --dnn_hidden_units 100 --dropout --batch_norm --data_dir cifar10/cifar-10-batches-py
+python3 train_mlp_pytorch.py --learning_rate 0.002 --dnn_hidden_units 100 --dropout --batch_norm --l1 --data_dir cifar10/cifar-10-batches-py
+python3 train_mlp_pytorch.py --learning_rate 0.002 --dnn_hidden_units 100,100 --dropout --batch_norm --data_dir cifar10/cifar-10-batches-py
+python3 train_mlp_pytorch.py --learning_rate 0.002 --dnn_hidden_units 100,100 --dropout --batch_norm --l1 --data_dir cifar10/cifar-10-batches-py
+
+
+python3 train_mlp_pytorch.py --learning_rate 0.002 --dnn_hidden_units 100 --dropout --data_dir cifar10/cifar-10-batches-py
+python3 train_mlp_pytorch.py --learning_rate 0.002 --dnn_hidden_units 100 --dropout --l1 --data_dir cifar10/cifar-10-batches-py
+python3 train_mlp_pytorch.py --learning_rate 0.002 --dnn_hidden_units 100,100 --dropout --data_dir cifar10/cifar-10-batches-py
+python3 train_mlp_pytorch.py --learning_rate 0.002 --dnn_hidden_units 100,100 --dropout --l1 --data_dir cifar10/cifar-10-batches-py
+
+python3 train_mlp_pytorch.py --learning_rate 0.002 --dnn_hidden_units 100 --batch_norm --data_dir cifar10/cifar-10-batches-py
+python3 train_mlp_pytorch.py --learning_rate 0.002 --dnn_hidden_units 100 --batch_norm --l1 --data_dir cifar10/cifar-10-batches-py
+python3 train_mlp_pytorch.py --learning_rate 0.002 --dnn_hidden_units 100,100 --batch_norm --data_dir cifar10/cifar-10-batches-py
+python3 train_mlp_pytorch.py --learning_rate 0.002 --dnn_hidden_units 100,100 --batch_norm --l1 --data_dir cifar10/cifar-10-batches-py
+
+python3 train_mlp_pytorch.py --learning_rate 0.002 --dnn_hidden_units 100  --data_dir cifar10/cifar-10-batches-py
+python3 train_mlp_pytorch.py --learning_rate 0.002 --dnn_hidden_units 100 --l1 --data_dir cifar10/cifar-10-batches-py
+python3 train_mlp_pytorch.py --learning_rate 0.002 --dnn_hidden_units 100,100 --data_dir cifar10/cifar-10-batches-py
+python3 train_mlp_pytorch.py --learning_rate 0.002 --dnn_hidden_units 100,100 --l1 --data_dir cifar10/cifar-10-batches-py
+
+
+python3 train_mlp_pytorch.py --learning_rate 0.02 --dnn_hidden_units 100 --dropout --data_dir cifar10/cifar-10-batches-py
+python3 train_mlp_pytorch.py --learning_rate 0.02 --dnn_hidden_units 100 --dropout --l1 --data_dir cifar10/cifar-10-batches-py
+python3 train_mlp_pytorch.py --learning_rate 0.02 --dnn_hidden_units 100,100 --dropout --data_dir cifar10/cifar-10-batches-py
+python3 train_mlp_pytorch.py --learning_rate 0.02 --dnn_hidden_units 100,100 --dropout --l1 --data_dir cifar10/cifar-10-batches-py
+
+python3 train_mlp_pytorch.py --learning_rate 0.02 --dnn_hidden_units 100 --dropout --batch_norm --data_dir cifar10/cifar-10-batches-py
+python3 train_mlp_pytorch.py --learning_rate 0.02 --dnn_hidden_units 100 --dropout --batch_norm --l1 --data_dir cifar10/cifar-10-batches-py
+python3 train_mlp_pytorch.py --learning_rate 0.02 --dnn_hidden_units 100,100 --dropout --batch_norm --data_dir cifar10/cifar-10-batches-py
+python3 train_mlp_pytorch.py --learning_rate 0.02 --dnn_hidden_units 100,100 --dropout --batch_norm --l1 --data_dir cifar10/cifar-10-batches-py
+
+python3 train_mlp_pytorch.py --learning_rate 0.02 --dnn_hidden_units 100 --batch_norm --data_dir cifar10/cifar-10-batches-py
+python3 train_mlp_pytorch.py --learning_rate 0.02 --dnn_hidden_units 100 --batch_norm --l1 --data_dir cifar10/cifar-10-batches-py
+python3 train_mlp_pytorch.py --learning_rate 0.02 --dnn_hidden_units 100,100 --batch_norm --data_dir cifar10/cifar-10-batches-py
+python3 train_mlp_pytorch.py --learning_rate 0.02 --dnn_hidden_units 100,100 --batch_norm --l1 --data_dir cifar10/cifar-10-batches-py
+
+python3 train_mlp_pytorch.py --learning_rate 0.02 --dnn_hidden_units 100  --data_dir cifar10/cifar-10-batches-py
+python3 train_mlp_pytorch.py --learning_rate 0.02 --dnn_hidden_units 100 --l1 --data_dir cifar10/cifar-10-batches-py
+python3 train_mlp_pytorch.py --learning_rate 0.02 --dnn_hidden_units 100,100 --data_dir cifar10/cifar-10-batches-py
+python3 train_mlp_pytorch.py --learning_rate 0.02 --dnn_hidden_units 100,100 --l1 --data_dir cifar10/cifar-10-batches-py
